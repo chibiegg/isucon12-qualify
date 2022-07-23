357de2c0005a7240f799a626f103ef8ae67c2e32
314@live.jp <__math>
2022-07-23 15:12:25 +0900

    optimize ranking

--- webapp/go/isuports.go
+++ webapp/go/isuports.go
@@ -13,6 +13,7 @@ import (
 	"path/filepath"
 	"reflect"
 	"regexp"
+	"sort"
 	"strconv"
 	"strings"
 	"time"
@@ -1069,6 +1070,17 @@ func competitionScoreHandler(c echo.Context) error {
 		psList = append(psList, ps)
 	}
 
+	// ranking用に事前にsortして、row_numにrankingを入れる
+	sort.Slice(psList, func(i, j int) bool {
+		if psList[i].Score == psList[j].Score {
+			return psList[i].RowNum < psList[j].RowNum
+		}
+		return psList[i].Score > psList[j].Score
+	})
+	for i := range psList {
+		psList[i].RowNum = int64(i + 1)
+	}
+
 	if _, err := tx.NamedExecContext(
 		ctx,
 		"INSERT INTO player_score (id, tenant_id, player_id, competition_id, score, row_num, created_at, updated_at) VALUES (:id, :tenant_id, :player_id, :competition_id, :score, :row_num, :created_at, :updated_at)",
@@ -1218,7 +1230,7 @@ func playerHandler(c echo.Context) error {
 }
 
 type CompetitionRank struct {
-	Rank              int64  `json:"rank"`
+	Rank              int64  `json:"rank" db:"rank"`
 	Score             int64  `json:"score" db:"score"`
 	PlayerID          string `json:"player_id" db:"player_id"`
 	PlayerDisplayName string `json:"player_display_name" db:"display_name"`
@@ -1288,21 +1300,16 @@ func competitionRankingHandler(c echo.Context) error {
 	if err := tx.SelectContext(
 		ctx,
 		&pagedRanks,
-		"SELECT p.display_name AS display_name, ps.player_id AS player_id, ps.score AS score FROM player_score as ps, player as p WHERE ps.tenant_id = ? AND ps.competition_id = ? AND ps.player_id = p.id ORDER BY ps.score DESC, ps.row_num ASC limit 100 offset ?",
+		"SELECT p.display_name AS display_name, ps.player_id AS player_id, ps.score AS score, ps.row_num as rank FROM player_score as ps, player as p WHERE ps.tenant_id = ? AND ps.competition_id = ? AND ? < ps.row_num AND ps.row_num <= ? AND ps.player_id = p.id ORDER BY ps.row_num ASC",
 		tenant.ID,
 		competitionID,
 		rankAfter,
+		rankAfter+100,
 	); err != nil {
 		tx.Rollback()
 		return fmt.Errorf("error Select player_score: tenantID=%d, competitionID=%s, %w", tenant.ID, competitionID, err)
 	}
 	tx.Commit()
-
-	respPagedRanks := make([]CompetitionRank, 0, len(pagedRanks))
-	for i, rank := range pagedRanks {
-		rank.Rank = rankAfter + int64(i+1)
-		respPagedRanks = append(respPagedRanks, rank)
-	}
 
 	res := SuccessResult{
 		Status: true,
@@ -1312,7 +1319,7 @@ func competitionRankingHandler(c echo.Context) error {
 				Title:      competition.Title,
 				IsFinished: competition.FinishedAt.Valid,
 			},
-			Ranks: respPagedRanks,
+			Ranks: pagedRanks,
 		},
 	}
 	return c.JSON(http.StatusOK, res)
