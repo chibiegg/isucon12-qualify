c1b741e22476d0097abc621daadf1ec81aaabc42
chibiegg <chibiegg@chibiegg.net>
2022-07-23 13:58:34 +0900

    init_tenat.sql

--- webapp/sql/init.sh
+++ webapp/sql/init.sh
@@ -4,6 +4,7 @@ set -ex
 cd `dirname $0`
 
 ISUCON_DB_HOST=${ISUCON_DB_HOST:-192.168.0.13}
+ISUCON_DB_HOST=${ISUCON_TENANT_DB_HOST:-192.168.0.12}
 ISUCON_DB_PORT=${ISUCON_DB_PORT:-3306}
 ISUCON_DB_USER=${ISUCON_DB_USER:-isucon}
 ISUCON_DB_PASSWORD=${ISUCON_DB_PASSWORD:-isucon}
@@ -15,6 +16,13 @@ mysql -u"$ISUCON_DB_USER" \
 		--host "$ISUCON_DB_HOST" \
 		--port "$ISUCON_DB_PORT" \
 		"$ISUCON_DB_NAME" < init.sql
+
+# MySQLを初期化
+mysql -u"$ISUCON_DB_USER" \
+		-p"$ISUCON_DB_PASSWORD" \
+		--host "$ISUCON_TENANT_DB_HOST" \
+		--port "$ISUCON_DB_PORT" \
+		"$ISUCON_DB_NAME" < init_tenant.sql
 
 # SQLiteのデータベースを初期化
 rm -f ../tenant_db/*.db
