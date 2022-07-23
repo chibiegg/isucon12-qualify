9052dc5baf8a36e35e013a9bdaa2cb561a001384
misodengaku <misodengaku@gmail.com>
2022-07-23 15:14:28 +0900

    playerHandler N+1

--- webapp/go/isuports.go
+++ webapp/go/isuports.go
@@ -1175,41 +1175,21 @@ func playerHandler(c echo.Context) error {
 		}
 		return fmt.Errorf("error retrievePlayer: %w", err)
 	}
-	cs := []CompetitionRow{}
+
+	pss := []PlayerScoreRow{}
 	if err := tenantDB.SelectContext(
 		ctx,
-		&cs,
-		"SELECT * FROM competition WHERE tenant_id = ? ORDER BY created_at ASC",
+		&pss,
+		"SELECT * FROM player_score WHERE tenant_id=? AND competition_id = ANY (SELECT id FROM competition WHERE tenant_id=?) AND player_id=?",
 		v.tenantID,
-	); err != nil && !errors.Is(err, sql.ErrNoRows) {
-		return fmt.Errorf("error Select competition: %w", err)
-	}
-
-	// player_scoreを読んでいるときに更新が走ると不整合が起こるのでロックを取得する (トランザクションにする)
-	tx := tenantDB.MustBegin()
-
-	pss := make([]PlayerScoreRow, 0, len(cs))
-	for _, c := range cs {
-		ps := PlayerScoreRow{}
-		if err := tx.GetContext(
-			ctx,
-			&ps,
-			// 最後にCSVに登場したスコアを採用する = row_numが一番大きいもの
-			"SELECT * FROM player_score WHERE tenant_id = ? AND competition_id = ? AND player_id = ? ORDER BY row_num DESC LIMIT 1",
-			v.tenantID,
-			c.ID,
-			p.ID,
-		); err != nil {
-			// 行がない = スコアが記録されてない
-			if errors.Is(err, sql.ErrNoRows) {
-				continue
-			}
-			tx.Rollback()
+		v.tenantID,
+		p.ID,
+	); err != nil {
+		// 行がない = スコアが記録されてない
+		if !errors.Is(err, sql.ErrNoRows) {
 			return fmt.Errorf("error Select player_score: tenantID=%d, competitionID=%s, playerID=%s, %w", v.tenantID, c.ID, p.ID, err)
 		}
-		pss = append(pss, ps)
 	}
-	tx.Commit()
 
 	psds := make([]PlayerScoreDetail, 0, len(pss))
 	for _, ps := range pss {
