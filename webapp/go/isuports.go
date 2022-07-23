6c95402638e0ed42f96ec0a4de924860f8aba5c7
314@live.jp <__math>
2022-07-23 14:05:10 +0900

    optimize competition-ranking-handler

--- webapp/go/isuports.go
+++ webapp/go/isuports.go
@@ -13,7 +13,6 @@ import (
 	"path/filepath"
 	"reflect"
 	"regexp"
-	"sort"
 	"strconv"
 	"strings"
 	"time"
@@ -1251,10 +1250,9 @@ func playerHandler(c echo.Context) error {
 
 type CompetitionRank struct {
 	Rank              int64  `json:"rank"`
-	Score             int64  `json:"score"`
-	PlayerID          string `json:"player_id"`
-	PlayerDisplayName string `json:"player_display_name"`
-	RowNum            int64  `json:"-"` // APIレスポンスのJSONには含まれない
+	Score             int64  `json:"score", db:"score"`
+	PlayerID          string `json:"player_id", db:"player_id"`
+	PlayerDisplayName string `json:"player_display_name", db:"display_name"`
 }
 
 type CompetitionRankingHandlerResult struct {
@@ -1319,57 +1317,22 @@ func competitionRankingHandler(c echo.Context) error {
 	if err != nil {
 		return fmt.Errorf("error flockByTenantID: %w", err)
 	}
+
 	defer fl.Close()
-	pss := []PlayerScoreRow{}
+	pagedRanks := []CompetitionRank{}
 	if err := tenantDB.SelectContext(
 		ctx,
-		&pss,
-		"SELECT * FROM player_score WHERE tenant_id = ? AND competition_id = ? ORDER BY row_num DESC",
+		&pagedRanks,
+		"SELECT p.display_name, ps.player_id, ps.score FROM player_score as ps, player as p WHERE ps.tenant_id = ? AND ps.competition_id = ? AND ps.player_id = p.id ORDER BY ps.score DESC, ps.row_num DESC limit 100 offset ?",
 		tenant.ID,
 		competitionID,
+		rankAfter,
 	); err != nil {
 		return fmt.Errorf("error Select player_score: tenantID=%d, competitionID=%s, %w", tenant.ID, competitionID, err)
 	}
-	ranks := make([]CompetitionRank, 0, len(pss))
-	scoredPlayerSet := make(map[string]struct{}, len(pss))
-	for _, ps := range pss {
-		// player_scoreが同一player_id内ではrow_numの降順でソートされているので
-		// 現れたのが2回目以降のplayer_idはより大きいrow_numでスコアが出ているとみなせる
-		if _, ok := scoredPlayerSet[ps.PlayerID]; ok {
-			continue
-		}
-		scoredPlayerSet[ps.PlayerID] = struct{}{}
-		p, err := retrievePlayer(ctx, tenantDB, ps.PlayerID)
-		if err != nil {
-			return fmt.Errorf("error retrievePlayer: %w", err)
-		}
-		ranks = append(ranks, CompetitionRank{
-			Score:             ps.Score,
-			PlayerID:          p.ID,
-			PlayerDisplayName: p.DisplayName,
-			RowNum:            ps.RowNum,
-		})
-	}
-	sort.Slice(ranks, func(i, j int) bool {
-		if ranks[i].Score == ranks[j].Score {
-			return ranks[i].RowNum < ranks[j].RowNum
-		}
-		return ranks[i].Score > ranks[j].Score
-	})
-	pagedRanks := make([]CompetitionRank, 0, 100)
-	for i, rank := range ranks {
-		if int64(i) < rankAfter {
-			continue
-		}
-		pagedRanks = append(pagedRanks, CompetitionRank{
-			Rank:              int64(i + 1),
-			Score:             rank.Score,
-			PlayerID:          rank.PlayerID,
-			PlayerDisplayName: rank.PlayerDisplayName,
-		})
-		if len(pagedRanks) >= 100 {
-			break
-		}
+
+	for i, rank := range pagedRanks {
+		rank.Rank = rankAfter + int64(i+1)
 	}
 
 	res := SuccessResult{
