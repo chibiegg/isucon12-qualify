74c0d700c9885c47454f0e86096f7ed54cb698f8
chibiegg <chibiegg@chibiegg.net>
2022-07-23 10:53:56 +0900

    tenant_id_competition_id_idx

--- webapp/sql/admin/10_schema.sql
+++ webapp/sql/admin/10_schema.sql
@@ -28,4 +28,5 @@ CREATE TABLE `visit_history` (
   `created_at` BIGINT NOT NULL,
   `updated_at` BIGINT NOT NULL,
   INDEX `tenant_id_idx` (`tenant_id`)
+  INDEX `tenant_id_competition_id_idx` (`tenant_id`, `competition_id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
