d3b21e1d4c88ba0014018a0cd0083fb182906227
chibiegg <chibiegg@chibiegg.net>
2022-07-23 13:10:22 +0900

    visit_history の INSERTに失敗しても無視する

--- webapp/go/isuports.go
+++ webapp/go/isuports.go
@@ -1365,16 +1365,12 @@ func competitionRankingHandler(c echo.Context) error {
 		return fmt.Errorf("error Select tenant: id=%d, %w", v.tenantID, err)
 	}
 
-	if _, err := adminDB.ExecContext(
+	// INSERT 失敗しても無視する
+	adminDB.ExecContext(
 		ctx,
 		"INSERT INTO visit_history (player_id, tenant_id, competition_id, created_at, updated_at) VALUES (?, ?, ?, ?, ?)",
 		v.playerID, tenant.ID, competitionID, now, now,
-	); err != nil {
-		return fmt.Errorf(
-			"error Insert visit_history: playerID=%s, tenantID=%d, competitionID=%s, createdAt=%d, updatedAt=%d, %w",
-			v.playerID, tenant.ID, competitionID, now, now, err,
-		)
-	}
+	)
 
 	var rankAfter int64
 	rankAfterStr := c.QueryParam("rank_after")
